a46de6750837f163ee26015b2bb631122a570ebb
Marcelo Rios <mrios@bevertec.com>
2021-08-07 05:17:44 -0500

    Adding elastic search logic

--- routes/products.js
+++ routes/products.js
@@ -6,10 +6,18 @@ const {
     loadProducts
 } = require('../controllers/products');
 
+const {
+    findProductsFromCatalog,
+    loadProductsIntoCatalog
+} = require('../api-controllers/products');
+
 const router = Router();
 
 
 router.get('/', getProducts);
 router.get('/load', loadProducts);
+
+router.get('/catalog', findProductsFromCatalog);
+router.get('/loadIntoCatalog', loadProductsIntoCatalog);
 
 module.exports = router;
