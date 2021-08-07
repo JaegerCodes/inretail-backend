abf80237bc7e37999ee42845e40018712e8620f0
Diego Ruiz <diego25061@gmail.com>
2021-08-07 03:18:04 -0500

    load fashion products by page

--- controllers/products.js
+++ controllers/products.js
@@ -10,12 +10,40 @@ const getProducts = async (req, res = response) => {
             products = response.data
         })
         .catch(error => {
+            res.status(500)
             console.error(error)
         });
 
     res.json(products)
 }
 
+const loadProducts = async (req, res = response) => {
+    let products = {}
+    const {
+        group = 0
+    } = req.query;
+
+    const fashionCat = 504
+    const groupSize = 50
+
+    await axios.get(process.env.OESCH_API_URL + "/catalog_system/pub/products/search" + "?fq=C:/" + fashionCat + "/&_from=" + group * groupSize + 1 + "&_to=" + (group + 1) * groupSize)
+        .then(response => {
+            products = response.data
+        })
+        .catch(error => {
+            res.status(500)
+            console.error(error)
+        });
+
+    res.json(products.map((e) => {
+        return {
+            product: e.productName,
+            categoryId: e.categoriesIds[0]
+        }
+    }))
+}
+
 module.exports = {
-    getProducts
+    getProducts,
+    loadProducts
 }
--- routes/products.js
+++ routes/products.js
@@ -2,12 +2,14 @@ const { Router } = require('express');
 let axios = require('axios').default;
 
 const {
-    getProducts
+    getProducts,
+    loadProducts
 } = require('../controllers/products');
 
 const router = Router();
 
 
 router.get('/', getProducts);
+router.get('/load', loadProducts);
 
 module.exports = router;
